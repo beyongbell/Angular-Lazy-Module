85492534ef59530d44e14974572d95e5413f822e
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-09-23 14:01:34 +0700

    Lazy Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'products',
+    loadChildren: './products/products.module#ProductsModule'
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
